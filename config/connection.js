8d80f187f02b8ba31a95659cfec1214cd776bcda
Josh Parker <jthomasparker@gmail.com>
2018-05-15 21:28:23 -0400

    testing db connection

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var mysql = require('mysql')
 
 var connection = mysql.createPool({
     host: "us-cdbr-iron-east-04.cleardb.net" || "localhost",
-    port: process.env.PORT || 3306,
+  //  port: process.env.PORT || 3306,
     user: "b14a9598a17f0a" || "root",
     password: "375092dc" || "jaz",
     database: "heroku_5f39a1926054948" || "burgers_db"
